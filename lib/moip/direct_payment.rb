35d56d29f26377bf8c086ba6ed368c2f142274ba
Marcelo Júnior <marcelo.machado.jr@gmail.com>
2012-05-10 21:35:02 -0300

    Ajustes

--- lib/moip/direct_payment.rb
+++ lib/moip/direct_payment.rb
@@ -3,33 +3,8 @@ require "nokogiri"
 
 module MoIP
 
-  # Baseado em http://labs.moip.com.br/pdfs/Integra%C3%A7%C3%A3o%20API%20-%20Autorizar%20e%20Cancelar%20Pagamentos.pdf
-  CodigoErro = 0..999
-  CodigoEstado = %w{AC AL AM AP BA CE DF ES GO MA MG MS MT PA PB PE PI PR RJ RN RO RR RS SC SE SP TO}
-  CodigoMoeda = "BRL"
-  CodigoPais = "BRA"
-  Destino = %w{Nenhum MesmoCobranca AInformar PreEstabelecido}
-  InstituicaoPagamento = %w{MoIP Visa AmericanExpress Mastercard Diners BancoDoBrasil Bradesco Itau BancoReal Unibanco Aura Hipercard Paggo Banrisul}
-  FormaPagamento = %w{CarteiraMoIP CartaoCredito CartaoDebito DebitoBancario FinanciamentoBancario BoletoBancario}
-  FormaRestricao = %w{Contador Valor}
-  PapelIndividuo = %w{Integrador Recebedor Comissionado Pagado}
-  OpcaoDisponivel = %w{Sim Não PagadorEscolhe}
-  Parcelador = %w{Nenhum Administradora MoIP Recebedor}
-  StatusLembrete = %w{Enviado Realizado EmAndamento Aguardando Falha}
-  StatusPagamento = %w{Concluido EmAnalise Autorizado Iniciado Cancelado BoletoImpresso Estornado}
-  TipoDias = %w{Corridos Uteis}
-  TipoDuracao = %w{Minutos Horas Dias Semanas Meses Ano}
-  TipoFrete = %w{Proprio Correio}
-  TipoIdentidade = %w{CPF CNPJ}
-  TipoInstrucao = %w{Unico Recorrente PrePago PosPago Remessa}
-  TipoLembrete = %w{Email SMS}
-  TipoPeriodicidade = %w{Anual Mensal Semanal Diaria}
-  TipoRecebimento = %w{AVista Parcelado}
-  TipoRestricao = %w{Autorizacao Pagamento}
-  TipoStatus = %w{Sucesso Falha}
-  
-  #
-  TiposComInstituicao = %w{CartaoCredito CartaoCredito DebitoBancario}
+  class MissingPaymentTypeError < StandardError ; end
+  class MissingPayerError < StandardError ; end
 
   class DirectPayment
 
@@ -37,23 +12,8 @@ module MoIP
 
       # Cria uma instrução de pagamento direto
       def body(attributes = {})
-
-#raise "#{attributes[:valor]}--#{attributes[:valor].to_f}"
         raise(MissingPaymentTypeError, "É necessário informar a razão do pagamento") if attributes[:razao].nil?
-        raise(MissingPayerError, "É obrigatório passar as informarções do pagador") if attributes[:pagador].nil?
-
-        raise(InvalidValue, "Valor deve ser maior que zero.") if attributes[:valor].to_f <= 0.0
-        raise(InvalidPhone, "Telefone deve ter o formato (99)9999-9999.") if attributes[:pagador][:tel_fixo] !~ /\(\d{2}\)?\d{4}-\d{4}/
-        raise(InvalidCellphone, "Telefone celular deve ter o formato (99)9999-9999.") if attributes[:pagador][:tel_cel] !~ /\(\d{2}\)?\d{4}-\d{4}/
-
-        raise(MissingBirthdate, "É obrigatório passar as informarções do pagador") if TiposComInstituicao.include?(attributes[:forma]) && attributes[:data_nascimento].nil?
-
-        raise(InvalidExpiry, "Data de expiração deve ter o formato 01-00 até 12-99.") if TiposComInstituicao.include?(attributes[:forma]) && attributes[:expiracao] !~ /(1[0-2]|0\d)\/\d{2}/
-
-
-        raise(InvalidReceiving, "Recebimento é inválido. Escolha um destes: #{TipoRecebimento.join(', ')}") if !TipoRecebimento.include?(attributes[:recebimento]) && TiposComInstituicao.include?(attributes[:forma])
-
-        raise(InvalidInstitution, "A instituição #{attributes[:instituicao]} é inválida. Escolha uma destas: #{InstituicaoPagamento.join(', ')}") if  TiposComInstituicao.include?(attributes[:forma]) && !InstituicaoPagamento.include?(attributes[:instituicao])
+        raise(MissingPayerError, "É obrigatório passar as informações do pagador") if attributes[:pagador].nil?
 
         builder = Nokogiri::XML::Builder.new(:encoding => "UTF-8") do |xml|
 
@@ -81,7 +41,7 @@ module MoIP
                 }
 
                 # Débito Bancário
-                if ["DebitoBancario"].include?(attributes[:forma])
+                if attributes[:forma] == "DebitoBancario"
                   xml.Instituicao {
                     xml.text attributes[:instituicao]
                   }
@@ -135,7 +95,7 @@ module MoIP
                 xml.Email { xml.text attributes[:pagador][:email] }
                 xml.TelefoneCelular { xml.text attributes[:pagador][:tel_cel] }
                 xml.Apelido { xml.text attributes[:pagador][:apelido] }
-                xml.Identidade(:Tipo => "CPF") { xml.text attributes[:pagador][:identidade] }
+                xml.Identidade { xml.text attributes[:pagador][:identidade] }
                 xml.EnderecoCobranca {
                   xml.Logradouro { xml.text attributes[:pagador][:logradouro] }
                   xml.Numero { xml.text attributes[:pagador][:numero] }
