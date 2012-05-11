e66bd804f0f9388609417f90c48ef68ea760c17e
Marcelo Júnior <marcelo.machado.jr@gmail.com>
2012-05-10 23:04:27 -0300

    Sincronização com plugin oficial

--- lib/moip/client.rb
+++ lib/moip/client.rb
@@ -3,7 +3,9 @@ require 'httparty'
 
 module MoIP
   class WebServerResponseError < StandardError ; end
+  class MissingConfigError < StandardError ; end
   class MissingTokenError < StandardError ; end
+  class MissingKeyError < StandardError ; end
 
   class Client
     include HTTParty
@@ -13,18 +15,42 @@ module MoIP
 
     class << self
 
+      # Verifica conta
+      # URL Produção: https://www.moip.com.br/ws/alpha/VerificarConta/{login_moip}
+      # URL SandBox: https://desenvolvedor.moip.com.br/sandbox/ws/alpha/VerificarConta/{login_moip}
+      # XML de Resposta:
+      # <ns1:verificarContaResponse>
+      #     <RespostaVerificarConta>
+      #         <Status>{status da conta}</Status>
+      #     </RespostaVerificarConta>
+      # </ns1:verificarContaResponse>
+      # 
+      # Valores Esperados: {status da conta}
+      # Inexistente " Login inexistente no sistema MoIP "
+      # Criado  " Login criado, porem não verificado "
+      # Verificado  " Login verificado "
+# MoIP::Client.verify 'aaa'
+
+      def verify account
+        full_data = peform_action!(:get, "VerificarConta/#{account}")
+        return full_data["ns1:verificarContaResponse"]["RespostaVerificarConta"]["Status"] == "Verificado"
+      end
+
       # Envia uma instrução para pagamento único
       def checkout(attributes = {})
-        full_data = peform_action!(:post, 'EnviarInstrucao/Unica', :body => DirectPayment.body(attributes))
-
-        get_response!(full_data["EnviarInstrucaoUnicaResponse"]["Resposta"])
+        body = DirectPayment.body(attributes)
+        puts "************ XML ************"
+        puts body
+        full_data = peform_action!(:post, 'EnviarInstrucao/Unica', :body => body)
+       # raise full_data.inspect
+        get_response!(full_data["ns1:EnviarInstrucaoUnicaResponse"]["Resposta"])
       end
 
       # Consulta dos dados das autorizações e pagamentos associados à Instrução
       def query(token)
         full_data = peform_action!(:get, "ConsultarInstrucao/#{token}")
 
-        get_response!(full_data["ConsultarTokenResponse"]["RespostaConsultar"])
+        get_response!(full_data["ns1:ConsultarTokenResponse"]["RespostaConsultar"])
       end
 
       # Retorna a URL de acesso ao MoIP
@@ -48,13 +74,22 @@ module MoIP
       private
 
       def peform_action!(action_name, url, options = {})
+
+        raise(MissingConfigError, "É necessário criar um arquivo de configuração para o moip. Veja mais em: https://github.com/moiplabs/moip-ruby") if MoIP.token.nil? && MoIP.key.nil?
+
+        raise(MissingTokenError, "É necessário informar um token na configuração") if MoIP.token.nil? || MoIP.token.empty?
+
+        raise(MissingKeyError, "É necessário informar um key na configuração") if MoIP.key.nil? || MoIP.key.empty?
+        
         response = self.send(action_name, url, options)
         raise(WebServerResponseError, "Ocorreu um erro ao chamar o webservice") if response.nil?
         response
       end
 
       def get_response!(data)
-        raise(WebServerResponseError, data["Erro"]) if data["Status"] == "Falha"
+        # raise data.inspect
+        err = data["Erro"].is_a?(Array) ? data["Erro"].join(", ") : data["Erro"]
+        raise(WebServerResponseError, err) if data["Status"] == "Falha"
         data
       end
     end
